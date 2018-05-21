230ae8fc2b28acc0ed44232ea8a584d3864df7a4
An Thien Tran <tranthienan.90@gmail.com>
2018-05-21 21:10:25 +0200

    Addecd links selector

---
+++ src/selectors/links.js
@@ -0,0 +1,26 @@
+// Manipulate the links array (sort, filter...)
+
+const sortLinks = (links = [], sortBy) => {
+    return links
+            .slice()
+            .sort((l1, l2) => {
+                if (sortBy === 'creation-date') {
+                    return l2.createdAt - l1.createdAt;
+                } else if (sortBy === 'last-visit') {
+                    if (l1.lastVisit && l2.lastVisit) {
+                        return l2.lastVisit - l1.lastVisit;
+                    } else if (l1.lastVisit) {
+                        return -1;
+                    } else if (l2.lastVisit) {
+                        return 1;
+                    } else {
+                        return 0;
+                    }
+                } else if (sortBy === 'visits') {
+                    return l2.visitCount - l1.visitCount;
+                }
+                return 0;
+            });
+};
+
+export default sortLinks;
