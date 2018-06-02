ac0afd105579edb4f9f98bbfa210b567ba7b16ca
An Thien Tran <tranthienan.90@gmail.com>
2018-06-02 13:15:41 +0200

    Added eslint config

--- src/index.js
+++ src/index.js
@@ -1,29 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 
-import App from './components/App';
-import configureStore from './store/configureStore';
-import { loadLinks } from './actions/links';
-import { setLoadingStatus } from './actions/loadingStatus';
-import registerServiceWorker from './registerServiceWorker';
-import 'normalize.css/normalize.css';
-import './styles/styles.css';
+import App from "./components/App";
+import configureStore from "./store/configureStore";
+import { loadLinks } from "./actions/links";
+import { setLoadingStatus } from "./actions/loadingStatus";
+import registerServiceWorker from "./registerServiceWorker";
+import "normalize.css/normalize.css";
+import "./styles/styles.css";
 
 const store = configureStore();
 
 const jsx = (
-    <Provider store={store}>
-        <App />
-    </Provider>
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
 
-store.dispatch(loadLinks())
-    .then(() => {
-        // set loading to false when done with loading the links
-        store.dispatch(setLoadingStatus(false));
-    });
+store.dispatch(loadLinks()).then(() => {
+  // set loading to false when done with loading the links
+  store.dispatch(setLoadingStatus(false));
+});
 
-ReactDOM.render(jsx, document.getElementById('root'));
+ReactDOM.render(jsx, document.getElementById("root"));
 
 registerServiceWorker();
